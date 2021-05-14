4247932c68778c7e1672d9f95d1572769a0184d0
Amitkumar Mishra <mr.mash@gmail.com>
2021-05-14 22:49:21 +0530

    added border on transaction card

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,6 +1,6 @@
 export default function Transaction({value}){
     return (
-        <div className="transaction">
+        <div className="transaction" style = {{borderLeft:value.type==='buy'?'5px solid #34aa33':'5px solid #f44646'}}>
             <p>{value.name} - {value.qty}@{value.currentPrice}</p>
             <p>{value.type === 'buy'?'Paid':'Received'}: ${value.value}</p>
             <p>Bought on: {value.timeStamp}</p>
