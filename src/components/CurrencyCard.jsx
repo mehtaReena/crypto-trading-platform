20fc17dcab361ada4ecca2d7278c6fda3a41d808
mehtaReena <j.reena07@gmail.com>
2021-05-14 18:00:17 +0530

    commit

--- src/components/CurrencyCard.jsx
+++ src/components/CurrencyCard.jsx
@@ -1,20 +1,28 @@
 
 
 function CurrencyCard(props) {
+
+    function clickHandler(){
+        props.method(props.index)
+
+    }
+
    console.log(  "CurrencyCard " + props)
     return (
-        <div className='currencycard'>
+        <div className='currencycard'  onClick={clickHandler}>
             <div className='currencyImage'>
             <img src={props.image} alt="bitcoin"></img>
             </div>
-            <div className='card-inf0'>
+            <div className='card-inf0'   >
 
-                <h3> {(props.percentageChange).toFixed(4)}</h3>
+                <h3> {(props.percentageChange).toFixed(5)}</h3>
                 <h4>{props.name}   </h4>
                 <p> last24h {props.currentPrice}</p>
 
 
             </div>
+
+
 
         </div>
     )
